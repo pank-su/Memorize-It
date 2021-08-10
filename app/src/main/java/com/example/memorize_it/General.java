75c5cd3bd802d6546e9a7ab2a13f3d7ddde2e855
pankov <frostkslo1@yandex.ru>
2021-08-10 15:20:45 +0300

    Что-то сделал

--- app/src/main/java/com/example/memorize_it/General.java
+++ app/src/main/java/com/example/memorize_it/General.java
@@ -1,6 +1,7 @@
 package com.example.memorize_it;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -26,11 +27,15 @@ public class General extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.mainActivity, R.id.navigation_notifications, R.id.navigation_setiings)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_general);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+    public void add_note(View v){
+        binding.navView.setSelectedItemId(R.id.mainActivity);
     }
 
 }
